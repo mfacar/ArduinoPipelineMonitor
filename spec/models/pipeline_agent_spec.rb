be42f4d291b26587d0d9e0433d9a2bba8b2ac99d
Mercy Falconi <mfalconi@thoughtworks.com>
2014-02-14 16:49:54 -0500

    [Ernesto-Mercy] Fixing pipeline agent

--- spec/models/pipeline_agent_spec.rb
+++ spec/models/pipeline_agent_spec.rb
@@ -4,22 +4,26 @@ describe PipelineAgent do
 
   describe "#process_status_update" do
     let(:valid_request) { {"monitor"=>{"project_name"=>"ernezto/feelme_server", "stage_name"=>"FastFeedback", "counter"=>36, "build_result"=>"Passed"}} }
-    let(:invalid_request) { {"invalid_parameter"=>{"project_name"=>""}} }
-    let(:mediator) { ServerContainer.pipeline_agent }
+    let(:pipeline_agent) { ServerContainer.pipeline_agent }
 
-    it "should return success message if request is valid" do
-      result = mediator.process_status_update(valid_request)
-      result.should == {success: true}
+    it "should create command according the monitor status" do
+      factory = double()
+      command = double().as_null_object
+      factory.should_receive(:create_command_from_request).with(valid_request).and_return(command)
+      pipeline_agent = PipelineAgent.new(factory, double().as_null_object)
+      pipeline_agent.process_status_update(valid_request)
     end
 
-    it "should return success false if request is not valid" do
-      result = mediator.process_status_update(invalid_request)
-      result.should == {success: false}
-    end
+    it "should execute command according the monitor status" do
+      factory = double()
+      command = double()
+      device = double()
+      factory.stub(:create_command_from_request).and_return(command)
+      command.should_receive(:execute).with(device)
 
-    xit "should update the device status according the monitor status" do
-      ArduinoDevice.any_instance.should_receive(:update).with(Enums::PipelineStatus::PASSED)
-      mediator.process_status_update(valid_request)
+      pipeline_agent = PipelineAgent.new(factory, device)
+
+      pipeline_agent.process_status_update(valid_request)
     end
   end
 
